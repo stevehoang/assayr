21932af1bfb5e99e1853687090f8d81126cc35bb
Nate <nathancday@gmail.com>
2017-06-13 16:10:16 -0400

    Added confint_builder()

---
+++ R/confint_builder.R
@@ -0,0 +1,43 @@
+library(magrittr)
+
+confint_builder <- function(mod, all = F) {
+    if (class(mod) == "lm") {
+        if (attributes(mod$terms)$intercept == 1) {
+            stop("Consider model with intercept = 0 for easier plotting")
+        }
+        if (length(attributes(mod$terms)$term.labels) > 1 & all == T) {
+            warning("More than 1 predicting variable, watch for extra rows in return or set all = F")
+        }
+        ci <- as.data.frame(confint(mod))
+        plot_pred <- attributes(mod$terms)$term.labels[1]
+        ci <- dplyr::mutate(ci, est = coef(mod), temp = gsub(plot_pred, "", rownames(ci)))
+        if (all == F) {
+            not_wanted <- attributes(mod$terms)$term.labels[-1]
+            not_wanted <- not_wanted[!grepl(plot_pred, not_wanted)]
+            for (p in not_wanted) {
+                ci %<>% dplyr::filter(!grepl(p, temp))
+            }
+        }
+    }
+    if (class(mod) == "merModLmerTest" | class(mod) == "lmerMod") {
+        num_sigma <- length(attributes(mod)$flist) + 1
+        ci <- as.data.frame(confint(mod))[-(1:num_sigma),]
+        if (rownames(ci)[1] == "(Intercept)") {
+            stop("Consider model with intercept = 0 for easier plotting")
+        }
+        ci$est <- nlme::fixef(mod)
+        plot_pred <- colnames(attributes(mod)$frame)[2]
+        ci$temp <- gsub(plot_pred, "", rownames(ci))
+        if (all == F) {
+            not_wanted <- colnames(attributes(mod)$frame)[c(-1,-2)]
+            not_wanted <- not_wanted[!grepl(plot_pred, not_wanted)]
+            for (p in not_wanted) {
+                ci %<>% dplyr::filter(!grepl(p, temp))
+            }
+        }
+    }
+    colnames(ci) <- c("lwr","upr", "est", plot_pred)
+    return(ci)
+}
+
+
