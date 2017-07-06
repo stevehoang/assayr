98a1b2345115e0b64cf6fc3f6a180710cc2ed452
Nate <nathancday@gmail.com>
2017-07-06 14:08:01 -0400

    fixed pipo

--- R/getMetaData.R
+++ R/getMetaData.R
@@ -1,35 +1,35 @@
 #' Import meta data
 #'
 #' Reads meta data from sample databases on Box via \code{boxr::box_read_excel()}
-#' 
+#'
 #' @param study_id Character object in the standard study name format (i.e. "RNO0101-1" or "RNO0101").
 #' @param fully_parse Logical indicating whether to try and filter the database to a specific run; Set \code{FALSE} to troubleshoot.
 #' @return A data frame containing the meta information from a given study or a large data frame with all the meta information from all studies in a program.
 #' @examples
 #' # returns only meta data from study RNO0738
 #' meta <- getMetaData("RNO0738")
-#' 
+#'
 #' # returns all studies' meta data from RNO07 project
-#' all_meta <- getMetaData("RNO0738", fully_parse = F) 
+#' all_meta <- getMetaData("RNO0738", fully_parse = F)
 #'@export
-getMetaData <- function(study_id, fully_parse = T) {
-    
+getMetaData <- function(study_id, fully_parse = TRUE) {
+
     program <- gsub("^(\\D{3}).*", "\\1", study_id)
-    
+
     box_files <- c(101838632791, 161176997524, 161176997524, 33980749645) %>%
         set_names(c("PAH", "PAU", "PAG", "RNO"))
     box_file <- box_files[program]
-    
+
     sheet_names <- c("PAH", "PAU_Huh7", "PAG_HepG2", "RNO07 Static") %>%
         set_names(c("PAH", "PAU", "PAG", "RNO"))
     box_which <- sheet_names[program]
-    
+
     meta <- box_read_excel(box_file, which = box_which, col_names = T)
-    
+
     if (fully_parse) {
-        meta %>% filter(`STUDY NAME` == study_id)
+        meta %<>% filter(`STUDY NAME` == study_id)
     }
-    
+
     return(meta)
 }
 
