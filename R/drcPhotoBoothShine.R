c38d92ed4bc259151da064f2e251eaa6db6f6f77
stevehoang <stephen.a.hoang@gmail.com>
2017-07-25 11:37:20 -0400

    tweak plots again

--- R/drcPhotoBoothShine.R
+++ R/drcPhotoBoothShine.R
@@ -144,8 +144,8 @@ drcPhotoBoothShine <- function(tib,
     ggplot2::scale_x_log10(breaks = c(0.01, 0.1, 1, 10, 100, 1000),
                   labels = scales::comma) +
     ggplot2::annotation_logticks(sides = "b") +
-    ggplot2::geom_point(size = 2) +
     ggplot2::geom_line(ggplot2::aes(x = xs, y = ys), data = curves, size = 1) +
+    ggplot2::geom_point(size = 2, alpha = 0.7) +
     ggplot2::geom_blank(ggplot2::aes(x=NULL), data = lims) +
     assayr::theme_assayr() +
     ggplot2::theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust=1)) +
