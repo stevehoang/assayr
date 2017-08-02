d342299def93667052b45cdd8a2eed9a47479340
stevehoang <stephen.a.hoang@gmail.com>
2017-08-02 17:32:03 -0400

    add dplyr::

--- R/coaPhotoBoothShine.R
+++ R/coaPhotoBoothShine.R
@@ -52,9 +52,9 @@ coaPhotoBoothShine <- function(tib,
 
   # Plot
   if (is.null(limits)) {
-    limits = list("Acetyl" = c(0, max(filter(tib, curve_plot == "Acetyl")[[y_var]])),
-                  "Isobutyryl" = c(0, max(filter(tib, curve_plot == "Isobutyryl")[[y_var]])),
-                  "Propionyl" = c(0, max(filter(tib, curve_plot == "Propionyl")[[y_var]])))
+    limits = list("Acetyl" = c(0, max(dplyr::filter(tib, curve_plot == "Acetyl")[[y_var]])),
+                  "Isobutyryl" = c(0, max(dplyr::filter(tib, curve_plot == "Isobutyryl")[[y_var]])),
+                  "Propionyl" = c(0, max(dplyr::filter(tib, curve_plot == "Propionyl")[[y_var]])))
   }
 
   cp <- dplyr::select(tib, tx_run, curve_plot) %>%
--- R/drcPhotoBoothShine.R
+++ R/drcPhotoBoothShine.R
@@ -138,8 +138,8 @@ drcPhotoBoothShine <- function(tib,
 
   # Plot
   if (is.null(limits)) {
-    limits = list("Isobutyryl" = c(0, max(filter(tib, curve_plot == "Isobutyryl")[[y_var]])),
-                  "Propionyl" = c(0, max(filter(tib, curve_plot == "Propionyl")[[y_var]])))
+    limits = list("Isobutyryl" = c(0, max(dplyr::filter(tib, curve_plot == "Isobutyryl")[[y_var]])),
+                  "Propionyl" = c(0, max(dplyr::filter(tib, curve_plot == "Propionyl")[[y_var]])))
   }
 
   cp <- dplyr::select(tib_dr, tx_run, curve_plot) %>%
