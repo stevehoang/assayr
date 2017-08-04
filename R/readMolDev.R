f3add78459f7a626ae09aa68fafe10a89f0335df
Nate <nathancday@gmail.com>
2017-08-04 17:04:01 -0400

    tweaked readMolDev for edgecase

--- R/readMolDev.R
+++ R/readMolDev.R
@@ -10,6 +10,7 @@
 readMolDev <- function(scope_txt, measurements = c("stell  nuc count", "mac nuc count", "smaa area",
                                                      "integrated int", "Nuclear Count",
                                                      "Vesicle", "Cell Count")) {
+    # first line of defence
     if (grepl("\\.txt", scope_txt)) {
         out_ncol_max <- max(count.fields(file = scope_txt, sep = "\t"))
         output <- read.table(file = scope_txt, sep = "\t", fill = T, col.names = 1:out_ncol_max, stringsAsFactors = F)
@@ -20,25 +21,30 @@ readMolDev <- function(scope_txt, measurements = c("stell  nuc count", "mac nuc 
     if (!grepl("xlsx|txt", scope_txt)) {
         stop("This function is only set up to handle xlsx or txt files as inputs")
     }
+    
     # output read based on ncols
     output <- as.data.frame(lapply(output, function(x) gsub(".\\dATF", "", x)), stringsAsFactors = F)
+    
     # grab meta info and plate dims
     meta_info <- grep(".*Name \\[Plate Info\\].*", output[,1], value = T)
     run_names <- gsub(".*=(\\D+\\d+-?\\d?).*", "\\1", meta_info)
     plate_nums <- gsub(".*Plate *(\\d*|\\d*b).*","\\1", meta_info, ignore.case = T)
     plate_starts <- grep("(Well Name|Plate ID)", output[,1]) + 1
-    plate_stops <- grep("Calculation applied", output[,1]) - 2
+    plate_stops <- grep("^6$", output[,1]) - 1
     plate_stops <- plate_stops[-1]
     plate_stops <- c(plate_stops, nrow(output))
+    
     # id row for variables of interest (ultimately colnames)
     id_row <- droplevels(output[(plate_starts[1]-1),])
     id_row <- lapply(id_row, as.character)
     well_name <- grep("Well Name", id_row)
     site_id <- grep("Site ID", id_row)
+    
     # based on measurement vector in function arguement
     measures <- sapply(measurements, grep, id_row) %>% unlist()
     columns_we_want <- c(well_name, site_id, measures)
     c_names <- id_row[columns_we_want]
+    
     # loop and make plates dfs
     plates <- data.frame(stringsAsFactors = F)
     for (p in 1:length(plate_starts)) {
@@ -52,15 +58,23 @@ readMolDev <- function(scope_txt, measurements = c("stell  nuc count", "mac nuc 
         p_df$plate <- p_num
         p_df$run <- r_name
         p_df %<>% dplyr::select(run, plate, everything())
-        plates <- rbind(plates, p_df)
+        
+        # in-case empty plates make it through
+        if (p_df[1,3] != "6") {
+            plates <- rbind(plates, p_df)
+        }
     }
     # polish colnames and uid
-    colnames(plates) <- make.names(colnames(plates)) %>% gsub("Cell", "", .) %>%
+    colnames(plates) <- make.names(colnames(plates)) %>%
+        gsub("Cell", "", .) %>%
         gsub("Custom.Module.", "", .) %>%
         gsub("\\.\\.", "", .)
     plates[,measures] %<>% sapply(function(x) as.numeric(x))
     plates$uid <- paste(plates$plate, plates$Well.Name, plates$Site.ID, sep = "_")
-    colnames(plates) %<>% gsub("^Well.*", "well", .) %>% gsub("^Site.*", "site", .) %>%
-        gsub("^Nuclear.*", "nuc_count", .) %>% gsub("^Vesicle.*", "nile_int", .)
+    colnames(plates) %<>% gsub("^Well.*", "well", .) %>%
+        gsub("^Site.*", "site", .) %>%
+        gsub("^Nuclear.*", "nuc_count", .) %>%
+        gsub("^Vesicle.*", "nile_int", .)
+    
     return(plates)
 }
