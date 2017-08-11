c6015323b0d5ff6fff3b4783716bc00d609360f8
Nate <nathancday@gmail.com>
2017-08-11 13:50:41 -0400

    update getMetaData for RSQ05 projects

--- R/getMetaData.R
+++ R/getMetaData.R
@@ -6,40 +6,48 @@
 #' @param fully_parse Logical indicating whether to try and filter the database to a specific run; Set \code{FALSE} to troubleshoot.
 #' @return A data frame containing the meta information from a given study or a large data frame with all the meta information from all studies in a program.
 #' @examples
+#' ## Requires boxR credentials
 #' # returns only meta data from study RNO0738
 #' meta <- getMetaData("RNO0738")
+#' # return meta data from multiple studies
 #' meta <- getMetaData(c("HEM0100-5", "HEM0100-6"))
 #'
 #' # returns all studies' meta data from RNO07 project
 #' all_meta <- getMetaData("RNO0738", fully_parse = F)
 #'@export
-getMetaData <- function(study_id, fully_parse = TRUE, static = TRUE) {
+getMetaData <- function(study_id, fully_parse = TRUE) {
     # manually adjusted for new experiments
-    box_files <- c(101838632791, 161176997524, 161176997524, 33980749645, 185483159790) %>%
-        set_names(c("PAH", "PAU", "PAG", "RNO", "HEM"))
-    sheet_names <- c("PAH", "PAU_Huh7", "PAG_HepG2", "RNO07 Static", "RNO20 (PBMC)", "HEM01") %>%
-        set_names(c("PAH", "PAU", "PAG", "RNO07", "RNO20", "HEM"))
-    
-    result <- tibble()
+    box_files <- c(101838632791, 161176997524, 161176997524, 33980749645,
+                   185483159790, 71120788417) %>%
+        set_names(c("PAH", "PAU", "PAG", "RNO",
+                    "HEM", "RSQ"))
+    # manually map for sheet names of interest
+    sheet_names <- c("PAH", "PAU_Huh7", "PAG_HepG2", "RNO07 Static",
+                     "RNO20 (PBMC)", "HEM01", "RSQ05 MF") %>%
+        set_names(c("PAH", "PAU", "PAG", "RNO07",
+                    "RNO20", "HEM", "RSQ05"))
+
+    result <- tibble() # container var
     for ( si in study_id ) {
-        
+
         program <- gsub("^(\\D{3}).*", "\\1", si)
-        
+
         box_file <- box_files[program]
-        
-        if ( program == "RNO") {
+
+        if ( program %in% c("RNO", "RSQ") ) {
             program <- gsub("^(\\D{3}\\d{2}).*", "\\1", si)
         }
-        
+
         box_which <- sheet_names[program]
-        
+
         meta <- box_read_excel(box_file, which = box_which, col_names = T, col_types = "text")
-        
+
         if (fully_parse) {
             meta %<>% filter(`STUDY NAME` == si)
         }
         result %<>% bind_rows(meta)
     }
+
     return(result)
 }
 
